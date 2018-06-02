c73d8d68819d01a184e76fbb3ab932655e78d747
LDiaz1241 <ldiaz1241@gmail.com>
2018-06-01 22:14:30 -0500

    Primera version del proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html><!--Este es un comentario-->
+	<head>
+		<title>Hola</title>
+	</head>
+	<body>
+		<header>
+			<h1>Titulo "h1"</h1>
+			<h2>Titulo "h2"</h2>
+			<h3>Titulo "h3"</h3>
+			<h4>Titulo "h4"</h4>
+			<h5>Titulo "h5"</h5>
+			<h6>Titulo "h6"</h6>
+		</header>
+		<section>
+			<img src="/images/pinguinos.jpg" alt="No se encontro la imagen"/>
+			<p>Parrafo</p>
+			<p><b>Parrafo negritas</b></p>
+			<p><strong>Parrafo negritas variable</strong></p>
+			<p><i>Parrafo italica</i></p>
+		</section>
+		<footer>
+			<p><u>Parrafo subrayado</u></p>
+			<p><s>Parrafo tachado</s></p>
+		</footer>
+	</body>
+</html>
